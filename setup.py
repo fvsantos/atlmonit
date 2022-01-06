1396cc203becec1d5f0069b0ee61ccf851589036
Francisco Santos <xikaovs@gmail.com>
2022-01-06 17:39:43 +0000

    test

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="atlmonit_1",
-    version="0.1.0",
+    name="atlmonit",
+    version="0.2.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
