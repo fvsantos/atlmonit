c65f9fe7563e6a4f464dce65c83bbf57597e4677
Francisco Santos <xikaovs@gmail.com>
2022-01-06 17:17:10 +0000

    test

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="atlmonit",
+    name="atlmonit_1",
     version="0.1.0",
     packages=find_packages(),
-    entry_points="""
-        [console_scripts]
-        atlmonit=atlmonit.main:cli
-    """,
+    entry_points={
+        "console_scripts": [
+            "atlmonit_2 = atlmonit.main:cli",
+        ],
+    },
 )
